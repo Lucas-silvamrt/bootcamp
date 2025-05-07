13f7890e5d02b9b6d636fc643fc63377cf9892c1
Lucas-silvamrt <lucassilva31102006@gmail.com>
2025-05-07 14:42:47 -0300

    Add files via upload

---
+++ def4.py
@@ -0,0 +1,9 @@
+print('um idoso esqueceu sua idade, usei def para calcular a idade do idoso')
+def idade (data,ano):
+    nasceu=calculo
+    return nasceu
+if __name__=='__main__':
+    data=int(input('que ano você nasceu?'))
+    ano=2025
+    calculo=ano-data
+    print(calculo)

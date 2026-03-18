3105b90e873e5e56c08ef6247adb2086006986c7
Lucas Silva <lucassilva31102006@gmail.com>
2026-03-18 14:46:07 -0300

    Delete def2.py

--- def2.py
+++
@@ -1,8 +0,0 @@
-def idade (data,ano):
-    nasceu=calculo
-    return idade 
-if __name__=='__main__':
-    data=int(input('que ano você nasceu?'))
-    ano=2025
-    calculo=ano-data
-    print(calculo)
